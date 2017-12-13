f325240a37de3afae6d5252180496e6a8feda410
zw <zwnibudongai@163.com>
2017-12-13 16:26:56 +0800

    修改解析扩展名，方便编辑器提示

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -32,7 +32,7 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js'],
     alias: {
       {{#if_eq build "standalone"}}
       'vue$': 'vue/dist/vue.esm.js',
